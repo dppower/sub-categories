c0088f0f01af1ad2f99dfac2508fc0bdbb19a34e
dppower <powerdp@gmail.com>
2018-04-13 21:23:48 +0100

    Added httpclient get

--- src/app/view-open-auctions/view-open-auctions.component.ts
+++ src/app/view-open-auctions/view-open-auctions.component.ts
@@ -3,40 +3,6 @@ import { Component, OnInit } from '@angular/core';
 import { Category } from "./categories";
 import { ViewOpenAuctionsService } from "./view-open-auctions.service";
 
-const test_categories = (<Category[]>[
-    { 
-        name: "root",
-        children: [1, 2]
-    },
-    {
-        name: "Category 1",
-        children: [3, 4]
-    },
-    {
-        name: "Category 2",
-        children: [5, 6]
-    },
-    {
-        name: "Child Category A",
-        children: [7, 8]
-    },
-    {
-        name: "Child Category B"
-    },
-    {
-        name: "Child Category C"
-    },
-    {
-        name: "Child Category D"
-    },
-    {
-        name: "Grandchild Catergory E"
-    },
-    {
-        name: "Grandchild Catergory F"
-    }
-]);
-
 @Component({
     selector: 'app-view-open-auctions',
     templateUrl: './view-open-auctions.component.html',
@@ -44,11 +10,11 @@ const test_categories = (<Category[]>[
 })
 export class ViewOpenAuctionsComponent implements OnInit {
 
-    categories: Category[] = test_categories;
+    categories: Category[] = [];
 
     constructor(private http_service_: ViewOpenAuctionsService) { };
 
     ngOnInit() {
-        //this.http_service_.getCategories().subscribe(categories => this.categories = categories);
+        this.http_service_.getCategories().subscribe(data => this.categories = data.categories);
     };
 }
--- src/app/view-open-auctions/view-open-auctions.service.ts
+++ src/app/view-open-auctions/view-open-auctions.service.ts
@@ -1,7 +1,43 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 
+import { catchError } from "rxjs/operators";
+import { of as rxOf } from "rxjs/observable/of";
 import { Category } from "./categories";
+
+const test_categories = (<Category[]>[
+    { 
+        name: "root",
+        children: [1, 2]
+    },
+    {
+        name: "Category 1",
+        children: [3, 4]
+    },
+    {
+        name: "Category 2",
+        children: [5, 6]
+    },
+    {
+        name: "Child Category A",
+        children: [7, 8]
+    },
+    {
+        name: "Child Category B"
+    },
+    {
+        name: "Child Category C"
+    },
+    {
+        name: "Child Category D"
+    },
+    {
+        name: "Grandchild Catergory E"
+    },
+    {
+        name: "Grandchild Catergory F"
+    }
+]);
 
 @Injectable()
 export class ViewOpenAuctionsService {
@@ -9,6 +45,10 @@ export class ViewOpenAuctionsService {
     constructor(private http_client_: HttpClient) { };
 
     getCategories() {
-        //return this.http_client_.get<Category[]>(""); //=> Put correct url
+        return this.http_client_.get<{ categories: Category[] }>("/test") //=> Put correct url
+            .pipe(
+                catchError((err) => rxOf<{ categories: Category[] }>({ categories: test_categories }) )
+            ); 
     };
+        
 }
